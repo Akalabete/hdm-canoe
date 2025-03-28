9ddc9e564667f02f39d1b1725ff6424bb3957815
Akalabete <lacour_alexandre@hotmail.com>
2025-03-28 10:06:56 +0100

    create / style / animate slider

--- script.js
+++ script.js
@@ -90,4 +90,122 @@ burger.addEventListener('click', () => {
         burger.style.height = 'auto';
     }
 });
+//4 carousel
 
+const carouselContainer = document.querySelector('.cover-photo-container');
+const carouselNext = document.querySelector('.carousel-next');
+const carouselPrev = document.querySelector('.carousel-prev');
+let currentIndex = 0;
+carouselNext.addEventListener('click', () => {
+    currentIndex = (currentIndex +1 + carouselImages.length) % carouselImages.length;
+    updateCarousel(currentIndex, 'next');
+    updateTimer()
+});
+
+carouselPrev.addEventListener('click', () => {
+    currentIndex = (currentIndex -1 + carouselImages.length) % carouselImages.length;
+    updateCarousel(currentIndex, 'prev');
+    updateTimer()
+});
+let carouselTimer;
+const carouselImages = [
+    {
+        src: './assets/images/slider/supp1opti.webp',
+        srcset: './assets/images/slider/supp1optism.webp 480w, ./assets/images/slider/supp1opti.webp 800w',
+        sizes: '(max-width: 600px) 480px, 800px',
+        alt: 'photo du débarcadère d\'Aubeterre Sur Drone'
+    },
+    {   
+        src: './assets/images/slider/supp3opti.webp',
+        srcset: './assets/images/slider/supp3optism.webp 480w, ./assets/images/slider/supp3opti.webp 800w',
+        sizes: '(max-width: 600px) 480px, 800px',
+        alt: 'photo du débarcadère d\'Aubeterre Sur Drone'
+    },
+    {
+        src: './assets/images/slider/p32opti.webp',
+        srcset: './assets/images/slider/p32optism.webp 480w, ./assets/images/slider/p32opti.webp 800w',
+        sizes: '(max-width: 600px) 480px, 800px',
+        alt: 'photo du débarcadère d\'Aubeterre Sur Drone'
+    },
+    {
+        src: './assets/images/slider/p36opti.webp',
+        srcset: './assets/images/slider/p36optism.webp 480w, ./assets/images/slider/p36opti.webp 800w',
+        sizes: '(max-width: 600px) 480px, 800px',
+        alt: 'photo du débarcadère d\'Aubeterre Sur Drone'
+    }
+]
+
+
+updateCarousel(0, 'start');
+updateTimer();
+
+
+function updateTimer() {
+    // Réinitialiser le timer précédent s'il existe
+    if (carouselTimer) {
+        clearInterval(carouselTimer);
+    }
+
+    // Créer un nouveau timer qui change d'image toutes les 5 secondes
+    carouselTimer = setInterval(() => {
+        currentIndex = (currentIndex + 1) % carouselImages.length;
+        updateCarousel(currentIndex, 'next');
+    }, 5000);
+}
+
+function updateCarousel(arg, direction) {
+    const carouselImg = carouselContainer.querySelector('img');
+    
+    if (carouselImg) {
+        
+        // Gestion des transitions
+        if(direction === 'start') {
+            injectImages(arg);
+        }
+        if (direction === 'next') {
+            //carouselImg.style.transition = 'transform 0.5s ease-in-out';
+            //carouselImg.style.transform = `translateX(100%)`;
+            carouselImg.style.transition = 'opacity 0.2s ease-in-out';
+            carouselImg.style.opacity = '0';
+            
+            
+            // Réinitialiser la position après la transition
+            setTimeout(() => {
+
+                //carouselImg.style.transition = 'none';
+               // carouselImg.style.transform = 'translateX(0)';
+                
+                injectImages(arg);
+                carouselImg.style.transition = 'opacity 0.3s ease-in-out';
+                carouselImg.style.opacity = '1';
+               
+                
+
+
+            }, 300);
+            
+        } else if (direction === 'prev') {
+            //carouselImg.style.transition = 'transform 0.5s ease-in-out';
+            //carouselImg.style.transform = `translateX(-100%)`;
+            carouselImg.style.transition = 'opacity 0.2s ease-in-out';
+            carouselImg.style.opacity = '0';
+            
+            // Réinitialiser la position après la transition
+            setTimeout(() => {
+                //carouselImg.style.transition = 'none';
+               //carouselImg.style.transform = 'translateX(0)';
+                injectImages(arg);
+                carouselImg.style.transition = 'opacity 0.3s ease-in-out';
+                carouselImg.style.opacity = '1';
+            }, 300);
+        }
+    }
+}
+
+function injectImages(index) {
+    const carouselImg = carouselContainer.querySelector('img');
+    carouselImg.src = carouselImages[index].src;
+    carouselImg.srcset = carouselImages[index].srcset;
+    carouselImg.sizes = carouselImages[index].sizes;
+    carouselImg.alt = carouselImages[index].alt;
+}
